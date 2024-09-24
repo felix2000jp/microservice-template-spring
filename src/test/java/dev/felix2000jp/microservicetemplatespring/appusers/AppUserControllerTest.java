1be1cc57f9c9bdbaa79638d54ead90dfe5952822
João Félix <61743088+felix2000jp@users.noreply.github.com>
2024-09-25 00:17:26 +0100

    feat: add build-and-test and semantic checks (#1)

--- src/test/java/dev/felix2000jp/microservicetemplatespring/appusers/AppUserControllerTest.java
+++ src/test/java/dev/felix2000jp/microservicetemplatespring/appusers/AppUserControllerTest.java
@@ -57,6 +57,19 @@ class AppUserControllerTest {
     }
 
     @Test
+    void getById_should_return_404_when_user_not_exists() throws Exception {
+        when(appUserService.findById(appUser.getId())).thenThrow(new AppUserNotFoundException());
+
+        mockMvc
+                .perform(get("/api/users/" + appUser.getId()))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("User was not found"));
+
+    }
+
+    @Test
     void getByUsername_should_return_200_and_user_when_user_exists() throws Exception {
         when(appUserService.findByUsername(appUser.getUsername())).thenReturn(appUser);
 
@@ -64,6 +77,18 @@ class AppUserControllerTest {
                 .perform(get("/api/users/username/" + appUser.getUsername()))
                 .andExpect(status().isOk())
                 .andExpect(content().json(appUserDtoJson));
+    }
+
+    @Test
+    void getByUsername_should_return_404_when_user_not_exists() throws Exception {
+        when(appUserService.findByUsername(appUser.getUsername())).thenThrow(new AppUserNotFoundException());
+
+        mockMvc
+                .perform(get("/api/users/username/" + appUser.getUsername()))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("User was not found"));
     }
 
     @Test
@@ -84,6 +109,25 @@ class AppUserControllerTest {
     }
 
     @Test
+    void create_should_return_409_when_user_not_unique() throws Exception {
+        when(appUserService.create(any(AppUser.class))).thenThrow(new AppUserConflictException());
+
+        var bodyJson = String.format("""
+                {
+                    "username": "%s",
+                    "password": "%s"
+                }
+                """, appUser.getUsername(), appUser.getPassword());
+
+        mockMvc
+                .perform(post("/api/users").contentType(MediaType.APPLICATION_JSON).content(bodyJson))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.title").value("Conflict"))
+                .andExpect(jsonPath("$.status").value(409))
+                .andExpect(jsonPath("$.detail").value("User already exists"));
+    }
+
+    @Test
     void update_should_return_204_when_user_updated() throws Exception {
         when(appUserService.update(any(AppUser.class))).thenReturn(appUser);
 
@@ -100,10 +144,61 @@ class AppUserControllerTest {
     }
 
     @Test
+    void update_should_return_404_when_user_not_exists() throws Exception {
+        when(appUserService.update(any(AppUser.class))).thenThrow(new AppUserNotFoundException());
+
+        var bodyJson = String.format("""
+                {
+                    "username": "%s",
+                    "password": "%s"
+                }
+                """, appUser.getUsername(), appUser.getPassword());
+
+        mockMvc
+                .perform(put("/api/users/" + appUser.getId()).contentType(MediaType.APPLICATION_JSON).content(bodyJson))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("User was not found"));
+    }
+
+    @Test
+    void update_should_return_409_when_user_not_unique() throws Exception {
+        when(appUserService.update(any(AppUser.class))).thenThrow(new AppUserConflictException());
+
+        var bodyJson = String.format("""
+                {
+                    "username": "%s",
+                    "password": "%s"
+                }
+                """, appUser.getUsername(), appUser.getPassword());
+
+        mockMvc
+                .perform(put("/api/users/" + appUser.getId()).contentType(MediaType.APPLICATION_JSON).content(bodyJson))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.title").value("Conflict"))
+                .andExpect(jsonPath("$.status").value(409))
+                .andExpect(jsonPath("$.detail").value("User already exists"));
+    }
+
+
+    @Test
     void delete_should_return_204_when_user_deleted() throws Exception {
         mockMvc
                 .perform(delete("/api/users/" + appUser.getId()))
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void delete_should_return_404_when_user_not_exists() throws Exception {
+        when(appUserService.delete(appUser.getId())).thenThrow(new AppUserNotFoundException());
+
+        mockMvc
+                .perform(delete("/api/users/" + appUser.getId()))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("User was not found"));
     }
 
 }
